195a2b4bb771d22e4a0af74487135aacfef54eb0
Aria Diniz <aria.diniz.dev@gmail.com>
2026-02-16 15:18:40 -0300

    creating trim and other string utils functions

---
+++ src/utils.c
@@ -0,0 +1,44 @@
+#include "../tge.h"
+#include <ctype.h>
+#include <stdlib.h>
+
+#define MAXLEN 64
+
+static unsigned int word_compare(char *restrict first, char *restrict second) {
+  for (int i = 0; i < MAXLEN && first[i] != '\0'; i++) {
+    if (tolower(first[i]) != tolower(second[i])) return 1;
+  }
+  return 0;
+}
+
+void tge_trim(char *str) {
+  char begin_flag = 0;
+  char begin_idx = 0;
+  char end_idx = 0;
+
+  for (unsigned int i = 0; i < MAXLEN && str[i] != '\0'; i++) {
+    if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || str[i] == '\r') {
+      if (begin_flag == 0) {
+        begin_idx++;
+        end_idx++;
+      }
+      continue;
+    }
+    begin_flag |= 0b1;
+    end_idx = i;
+  }
+
+  for (unsigned int i = begin_idx; i <= end_idx; i++) {
+    str[i - begin_idx] = str[i];
+  }
+
+  str[end_idx - begin_idx + 1] = '\0';
+}
+
+static char **command_array(char *command) {
+
+}
+
+extern void run_action(char *command, tge_action_func action) {
+  
+}
---
+++ tge.h
@@ -0,0 +1,40 @@
+#ifndef __LIB_TGE_HEADER_
+#define __LIB_TGE_HEADER_
+
+#include <stdlib.h>
+
+typedef void *tge_action_func(
+  char *verb,
+  char *noun,
+  char *prep,
+  char *ind_obj
+);
+
+typedef struct tge_command {
+  char *verb;
+  char *noun;
+  char *preposition;
+  char *indirect_obj;
+  void *action_func;
+} tge_command;
+
+typedef struct tge_room {
+  char *description;
+  char **items;
+  struct tge_room **adjacent_rooms;
+} tge_room;
+
+typedef struct tge_player {
+  char *name;
+  char **pronouns;
+  char **items;
+  char **status;
+  int max_health;
+  int current_health;
+  struct tge_room *current_room;
+} tge_player;
+
+void run_action(char *command, tge_action_func action);
+void tge_trim(char *str);
+
+#endif
