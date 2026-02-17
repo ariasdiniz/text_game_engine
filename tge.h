6f1ae66d5cb6ce65d723c69a246b85cc9739306e
Aria Diniz <aria.diniz.dev@gmail.com>
2026-02-17 11:45:23 -0300

    standardizing sizes

--- tge.h
+++ tge.h
@@ -3,6 +3,11 @@
 
 #include <stdlib.h>
 
+#define MAX_DESC_LEN 2048
+#define MAX_SMAL_DESC_LEN 64
+#define MAX_INVENTORY_SIZE 32
+#define MAX_NAME_LEN 16
+
 typedef struct tge_command {
   char *verb;
   char *noun;
@@ -24,16 +29,17 @@ typedef struct tge_command_special_words {
 typedef void tge_action_func(tge_command *cmd);
 
 typedef struct tge_room {
-  char *description;
-  char **items;
+  char description[MAX_DESC_LEN];
+  char small_description[MAX_SMAL_DESC_LEN];
+  char items[MAX_INVENTORY_SIZE][MAX_NAME_LEN];
   struct tge_room **adjacent_rooms;
 } tge_room;
 
 typedef struct tge_player {
-  char *name;
-  char **pronouns;
-  char **items;
-  char **status;
+  char name[MAX_NAME_LEN];
+  char pronouns[2][MAX_NAME_LEN];
+  char items[MAX_INVENTORY_SIZE][MAX_NAME_LEN];
+  char status[MAX_INVENTORY_SIZE][MAX_NAME_LEN];
   int max_health;
   int current_health;
   struct tge_room *current_room;
